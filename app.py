5ffab022f557f0184a2bbeacf5aead86e6bfb036
JakelynDuarte <jakelynduarte0902@gmail.com>
2026-02-27 13:24:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify, make_response, session
+from flask import Flask, 
+render_template, request, jsonify, make_response, session
 app = Flask(__name__)
 
 @app.route('/clientes')
