5e90555416ad12c1b7b4de7c032003467b6be573
JakelynDuarte <jakelynduarte0902@gmail.com>
2026-02-27 13:10:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, make_response, session
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/clientes')
+def clientes():
+   import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user=" u760464709_dam_repo4a_usr",
+        password="5BYtj5fxKLs;",
+        database="u760464709_dam_repo4a_bd"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM clientes")
+    myresult = mycursor.fetchall()
+    return make_response(jsonify(myresult))
+    
+
