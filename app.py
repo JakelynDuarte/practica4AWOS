7381f4d273247ac2e1d0fcd70483406b2dfb774c
JakelynDuarte <jakelynduarte0902@gmail.com>
2026-02-27 20:07:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify, make_response, session
+from flask_cors import CORS
 import mysql.connector
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/clientes')
 def clientes():
@@ -11,9 +13,34 @@ def clientes():
         password="5BYtj5fxKLs;",
         database="u760464709_dam_repo4a_bd"
     )
+
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM clientes")
     myresult = mycursor.fetchall()
-    return make_response(jsonify(myresult))
-    
 
+    return make_response(jsonify(myresult))
+
+
+@app.post('/cliente')
+def cliente():
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_dam_repo4a_usr",
+        password="5BYtj5fxKLs;",
+        database="u760464709_dam_repo4a_bd"
+    )
+
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO clientes (idCliente, nombreCliente, numero) VALUES (%s, %s, %s)"
+    
+    val = (
+        request.form['txtCliente'],
+        request.form['cboNombreCliente'],
+        request.form['txtnumero']
+    )
+
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    return "correcto"
