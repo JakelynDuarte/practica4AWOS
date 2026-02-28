5b29f1e8613ff06f3c096ec93f04f5a00aec6ca4
JakelynDuarte <jakelynduarte0902@gmail.com>
2026-02-27 18:56:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, jsonify, make_response, session
+import mysql.connector
+
 app = Flask(__name__)
 
 @app.route('/clientes')
 def clientes():
-   import mysql.connector
     mydb = mysql.connector.connect(
         host="46.28.42.226",
-        user=" u760464709_dam_repo4a_usr",
+        user="u760464709_dam_repo4a_usr",
         password="5BYtj5fxKLs;",
         database="u760464709_dam_repo4a_bd"
     )
