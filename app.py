327523fbde2da09af992705cd334a6df560dd175
JakelynDuarte <jakelynduarte0902@gmail.com>
2026-02-27 20:22:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,10 @@ def clientes():
         password="5BYtj5fxKLs;",
         database="u760464709_dam_repo4a_bd"
     )
-
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM clientes")
     myresult = mycursor.fetchall()
-
     return make_response(jsonify(myresult))
-
 
 @app.post('/cliente')
 def cliente():
@@ -29,18 +26,47 @@ def cliente():
         password="5BYtj5fxKLs;",
         database="u760464709_dam_repo4a_bd"
     )
-
     mycursor = mydb.cursor()
-
     sql = "INSERT INTO clientes (idCliente, nombreCliente, numero) VALUES (%s, %s, %s)"
-    
     val = (
         request.form['txtCliente'],
         request.form['cboNombreCliente'],
         request.form['txtnumero']
     )
-
     mycursor.execute(sql, val)
     mydb.commit()
-
     return "correcto"
+
+@app.put('/cliente')
+def actualizar_cliente():
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_dam_repo4a_usr",
+        password="5BYtj5fxKLs;",
+        database="u760464709_dam_repo4a_bd"
+    )
+    mycursor = mydb.cursor()
+    sql = "UPDATE clientes SET nombreCliente = %s, numero = %s WHERE idCliente = %s"
+    val = (
+        request.form['cboNombreCliente'],
+        request.form['txtnumero'],
+        request.form['txtCliente']
+    )
+    mycursor.execute(sql, val)
+    mydb.commit()
+    return "actualizado"
+
+@app.delete('/cliente/<id>')
+def eliminar_cliente(id):
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_dam_repo4a_usr",
+        password="5BYtj5fxKLs;",
+        database="u760464709_dam_repo4a_bd"
+    )
+    mycursor = mydb.cursor()
+    sql = "DELETE FROM clientes WHERE idCliente = %s"
+    val = (id,)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    return "eliminado"
